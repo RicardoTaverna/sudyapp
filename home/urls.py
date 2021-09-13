821958effa9e884b73a8cfb5f2037cf135ee0783
Ricardo Tavera <taverna.ricardo@gmail.com>
2021-09-13 15:26:17 -0300

    add pagina de perfil

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('', include('django.contrib.auth.urls')),
     path('signup/', views.signup, name='signup'),
-    path('accounts/profile/', name='profile')
+    path('accounts/profile/', views.profile, name='profile')
 ]
