7ac1a912f1283ec938f3dcce6504be089eecd499
Ricardo Tavera <taverna.ricardo@gmail.com>
2021-09-13 15:59:48 -0300

    trocando template

--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def home(request):
-    return render(request, 'home/home.html')
+    return render(request, 'index.html')
 
 
 def signup(request):
